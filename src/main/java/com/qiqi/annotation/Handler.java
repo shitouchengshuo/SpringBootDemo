ebb4a346bc669152eef82b8da426d8b995fea131
new company <qiqi.zhao@phicomm.com>
2019-01-26 11:25:31 +0800

    @Handler注解目前没有使用

--- src/main/java/com/qiqi/annotation/Handler.java
+++ src/main/java/com/qiqi/annotation/Handler.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 import java.lang.annotation.*;
 
 /**
- * 自定义注解: instruction:指令类型
+ * 自定义注解: instruction:指令类型 (目前没使用该注解)
  */
 @Target(ElementType.TYPE)//对类或接口等的注解
 @Retention(RetentionPolicy.RUNTIME) //运行时注解依然会存在
